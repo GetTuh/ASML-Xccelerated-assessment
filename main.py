1743e631d2e50e210cf7429c92ca6dfd814df551
GetTuh <jakubniemczyk97@gmail.com>
2022-06-12 17:12:22 +0200

    Fetching values for backup-1 mode

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
+import sys
 from google.cloud import compute_v1
 
 def fetch_vms(project, zone):
-    
-    c = compute_v1.InstancesClient.from_service_account_json("xcc-assessment-jakub.json")
-    print(c)
+    c = compute_v1.InstancesClient.from_service_account_json(
+        "xcc-assessment-jakub.json")
     instances = c.list(project=project, zone=zone)
     return instances
 
 
-def fetch_snapshots(projects):
-    c = SnapshotsClient.from_service_account_json("xcc-assessment-jakub.json")
+def fetch_snapshots(project):
+    c = compute_v1.SnapshotsClient.from_service_account_json("xcc-assessment-jakub.json")
     snapshots = c.list(project=project)
     return snapshots
 
-fetch_vms("xcc-assessment-jakub", "europe-west4-a")
+
+def backup1(project,zone):
+    vms = fetch_vms(project, zone)
+    for vm in vms.items:
+        print(vm.name)
+        print(vm.labels['backup'])
+        print(vm.disks[0].source)
+        if(vm.labels['backup']):
+            snapshots=fetch_snapshots(project)
+            print(snapshots.items[0].creation_timestamp)
+
+
+
+def main(mode, zone=0):
+    project = 'xcc-assessment-jakub'
+    if(mode == "backup-1"):
+        backup1(project, zone)
+       
+
+main(str(sys.argv[1]),str(sys.argv[2]))
