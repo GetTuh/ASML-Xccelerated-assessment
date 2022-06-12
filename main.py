fd6a3cd2fb07b595dbfbf94776601ae4a6f2472f
GetTuh <jakubniemczyk97@gmail.com>
2022-06-12 14:51:55 +0200

    Added requirements.txt & authenticating

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-def fetch_vms(project,zone):
-    c=InstancesClient.from_service_account_json(".json")
+from google.cloud import compute_v1
+
+def fetch_vms(project, zone):
+    
+    c = compute_v1.InstancesClient.from_service_account_json("xcc-assessment-jakub.json")
+    print(c)
     instances = c.list(project=project, zone=zone)
     return instances
 
+
 def fetch_snapshots(projects):
-    c=SnapshotsClient.from_service_account_json(".json")
+    c = SnapshotsClient.from_service_account_json("xcc-assessment-jakub.json")
     snapshots = c.list(project=project)
     return snapshots
+
+fetch_vms("xcc-assessment-jakub", "europe-west4-a")
