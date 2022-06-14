7ea82fb083b1c18d2c0e8a33174fc4eb9227058e
GetTuh <jakubniemczyk97@gmail.com>
2022-06-14 15:11:18 +0200

    Added backup2 core functionality

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ def fetch_snapshots(project):
     return snapshots
 
 
+def create_snapshot(project, zone, disk):
+    c = compute_v1.SnapshotsClient.from_service_account_json(
+        "xcc-assessment-jakub.json")
+    snap = {
+        'name': 'backup-instance',
+        'source_disk': disk.source
+    }
+    response = c.insert(project=project, snapshot_resource=snap)
+
+
+def delete_snapshot(project, snapshot):
+    c = compute_v1.SnapshotsClient.from_service_account_json(
+        "xcc-assessment-jakub.json")
+    c.delete(project=project, snapshot=snapshot)
+
+
 def backup1(project, zone):
     vms = fetch_vms(project, zone)
     tableData = []
@@ -34,10 +50,29 @@ def backup1(project, zone):
           "Instance", "Backup Enabled", "Disk", "Last Backup"]))
 
 
+def backup2(project, zone):
+    vms = fetch_vms(project, zone)
+    for vm in vms.items:
+        if(vm.labels['backup'] == 'true'):
+            create_snapshot(project, zone, vm.disks[0])
+
+
+def backup3(project, zone):
+    return 0
+
+
 def main(mode, zone=0):
     project = 'xcc-assessment-jakub'
     if(mode == "backup-1"):
         backup1(project, zone)
+    if(mode == 'backup-2'):
+        backup2(project, zone)
+    if(mode == 'backup-3'):
+        snapshot = "initial-snapshot"
+        backup3(project, snapshot)
 
 
-main(str(sys.argv[1]), str(sys.argv[2]))
+if(len(str(sys.argv)) == 3):
+    main(str(sys.argv[1]), str(sys.argv[2]))
+else:
+    main(str(sys.argv[1]), 'europe-west4-a')
